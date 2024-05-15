d667ea4ca9d07305b1d3ff1b590a88c8d5aae89b
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-15 22:58:26 +0900

    [feat] make title View

--- PrimePickApp/ContentView.swift
+++ PrimePickApp/ContentView.swift
@@ -10,12 +10,64 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("PrimePick")
+                .font(.custom("Helvetica Neue", size: 40))
+                .fontWeight(.bold)
+                .padding(.top, 100)
+                .padding(.bottom, 30)
+            
+            Spacer()
+            
+            Text("Select Difficulty")
+                .font(.headline)
+                .padding()
+            
+            Button(action: {
+                // Hardボタンのアクション
+            }) {
+                Text("Hard")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+            }
+            .padding(.horizontal, 50)
+            .padding(.bottom, 10)
+            
+            Button(action: {
+                // Normalボタンのアクション
+            }) {
+                Text("Normal")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+            }
+            .padding(.horizontal, 50)
+            .padding(.bottom, 10)
+            
+            Button(action: {
+                // Easyボタンのアクション
+            }) {
+                Text("Easy")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+            }
+            .padding(.horizontal, 50)
+            
+            Spacer()
         }
-        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
