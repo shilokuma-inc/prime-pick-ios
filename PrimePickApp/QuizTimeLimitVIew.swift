ffba725e655c3efc4ce14a8be387f1143d07e466
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-21 18:25:27 +0900

    [feat] add QuizTimeLimitVIew background

--- PrimePickApp/QuizTimeLimitVIew.swift
+++ PrimePickApp/QuizTimeLimitVIew.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct QuizTimeLimitVIew: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.gray
+                .opacity(0.5)
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
