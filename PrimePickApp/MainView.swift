44c8c0220f3a549c0bec9d1bd4aaa47101c51142
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-22 23:32:25 +0900

    [feat] ボタンのデザインをリッチに

--- PrimePickApp/MainView.swift
+++ PrimePickApp/MainView.swift
@@ -29,35 +29,66 @@ struct MainView: View {
                     
                     NavigationLink(destination: LazyView(QuizView(difficulty: "Hard"))) {
                         Text("Hard")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
                             .padding()
                             .frame(maxWidth: .infinity)
-                            .background(Color.red)
+                            .background(
+                                LinearGradient(gradient: Gradient(colors: [Color.red, Color.purple]),
+                                               startPoint: .topLeading,
+                                               endPoint: .bottomTrailing)
+                            )
                             .foregroundColor(.white)
                             .cornerRadius(20)
+                            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 5, y: 5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.white.opacity(0.7), lineWidth: 2)
+                            )
                     }
                     .padding(.horizontal, 50)
                     .padding(.bottom, 10)
                     
                     NavigationLink(destination: LazyView(QuizView(difficulty: "Normal"))) {
                         Text("Normal")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
                             .padding()
                             .frame(maxWidth: .infinity)
-                            .background(Color.blue)
+                            .background(
+                                LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]),
+                                               startPoint: .topLeading,
+                                               endPoint: .bottomTrailing)
+                            )
                             .foregroundColor(.white)
                             .cornerRadius(20)
+                            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 5, y: 5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.white.opacity(0.7), lineWidth: 2)
+                            )
                     }
                     .padding(.horizontal, 50)
                     .padding(.bottom, 10)
                     
                     NavigationLink(destination: LazyView(QuizView(difficulty: "Easy"))) {
                         Text("Easy")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
                             .padding()
                             .frame(maxWidth: .infinity)
-                            .background(Color.green)
+                            .background(
+                                LinearGradient(gradient: Gradient(colors: [Color.green, Color.yellow]),
+                                               startPoint: .topLeading,
+                                               endPoint: .bottomTrailing)
+                            )
                             .foregroundColor(.white)
                             .cornerRadius(20)
+                            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 5, y: 5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color.white.opacity(0.7), lineWidth: 2)
+                            )
                     }
                     .padding(.horizontal, 50)
+                    .padding(.bottom, 10)
                     
                     Spacer()
                 }
