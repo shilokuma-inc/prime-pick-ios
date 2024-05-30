f850b2699ca5f39770320f194d3a04f69710a58c
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-31 01:22:44 +0900

    [feat] 結果画面を作成

--- PrimePickApp/View/Quiz/QuizResultView.swift
+++ PrimePickApp/View/Quiz/QuizResultView.swift
@@ -9,26 +9,56 @@ import SwiftUI
 
 struct QuizResultView: View {
     @Environment(\.dismiss) private var dismiss
+    @State private var rainbowColor: Color = .red
+    @State private var animationAngle: Double = 0
     let score: Int
     
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Color.green
+                Color.appBackground
                     .frame(
                         width: geometry.size.width * 2 / 3,
                         height: geometry.size.height / 2
                     )
+                    .cornerRadius(20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(rainbowColor, lineWidth: 5)
+                    )
+                    .onAppear {
+                        withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                            animationAngle = 360
+                        }
+                    }
                 
                 Text("Your Score is \(score) points!")
+                    .frame(
+                        width: geometry.size.width / 2,
+                        height: geometry.size.height / 2
+                    )
+                    .font(.title)
+                    .multilineTextAlignment(.center)
             }
-            .frame(
-                width: geometry.size.width,
-                height: geometry.size.height
-            )
+            .frame(width: geometry.size.width, height: geometry.size.height)
         }
         .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            startColorAnimation()
+        }
     }
+    
+    private func startColorAnimation() {
+            let colors: [Color] = [.red, .orange, .yellow, .green, .blue, .purple]
+            var currentIndex = 0
+
+            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
+                withAnimation {
+                    rainbowColor = colors[currentIndex]
+                }
+                currentIndex = (currentIndex + 1) % colors.count
+            }
+        }
 }
 
 #Preview {
