61b0a8428e14f513967257728b6acc54abb0474c
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-31 00:46:07 +0900

    [feat] make QuizResultView

---
+++ PrimePickApp/View/Quiz/QuizResultView.swift
@@ -0,0 +1,20 @@
+//
+//  QuizResultView.swift
+//  PrimePickApp
+//
+//  Created by 村石 拓海 on 2024/05/31.
+//
+
+import SwiftUI
+
+struct QuizResultView: View {
+    let score: Int
+    
+    var body: some View {
+        Text("You Score is \(score) points!")
+    }
+}
+
+#Preview {
+    QuizResultView(score: 1)
+}
