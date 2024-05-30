1f93ee8f8fdf58377aee5e8d32096e6bb7ab4822
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-31 02:57:03 +0900

    [refactor] make QuizContentView

---
+++ PrimePickApp/View/Quiz/QuizContent/QuizContentView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizContentView.swift
+//  PrimePickApp
+//
+//  Created by 村石 拓海 on 2024/05/31.
+//
+
+import SwiftUI
+
+struct QuizContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizContentView()
+}
