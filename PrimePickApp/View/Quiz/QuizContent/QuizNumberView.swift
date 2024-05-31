c0a7a762660f4956177dcb6a373b83f27bf34c86
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-31 21:07:13 +0900

    [refactor] if式を適用

--- PrimePickApp/View/Quiz/QuizContent/QuizNumberView.swift
+++ PrimePickApp/View/Quiz/QuizContent/QuizNumberView.swift
@@ -31,34 +31,21 @@ struct QuizNumberView: View {
 }
 
 private func quizNumberBackgroundView(difficulty: Difficulty) -> some View {
-    switch difficulty {
-    case .easy:
-        RoundedRectangle(cornerRadius: 25)
-            .stroke(Color.green, lineWidth: 5)
-            .background(
-                RoundedRectangle(cornerRadius: 25).fill(Color.white)
-            )
-            .frame(width: 300, height: 200)
-            .shadow(radius: 10)
-        
-    case .normal:
-        RoundedRectangle(cornerRadius: 25)
-            .stroke(Color.blue, lineWidth: 5)
-            .background(
-                RoundedRectangle(cornerRadius: 25).fill(Color.white)
-            )
-            .frame(width: 300, height: 200)
-            .shadow(radius: 10)
-        
-    case .hard:
-        RoundedRectangle(cornerRadius: 25)
-            .stroke(Color.red, lineWidth: 5)
-            .background(
-                RoundedRectangle(cornerRadius: 25).fill(Color.white)
-            )
-            .frame(width: 300, height: 200)
-            .shadow(radius: 10)
+    let color = if difficulty == .easy {
+        Color.appGreen
+    } else if difficulty == .normal {
+        Color.blue
+    } else {
+        Color.red
     }
+    
+    return RoundedRectangle(cornerRadius: 25)
+        .stroke(color, lineWidth: 5)
+        .frame(width: 300, height: 200)
+        .shadow(radius: 10)
+        .background(
+            RoundedRectangle(cornerRadius: 25).fill(Color.white)
+        )
 }
 
 struct QuizNumberView_Previews: PreviewProvider {
